4028f45d0f55b9c0136b6b5a40de14bcd9c0dc02
Eslam Akram <eslam_akram91@yahoo.com>
2021-07-30 17:04:10 +0300

    updated

--- js/App.js
+++ js/App.js
@@ -130,6 +130,6 @@ return [multiplication,`The numbers ${dynamicArray[0]},${dynamicArray[1]},${dyna
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
- testMultiplyAnyArray(testDynamicArray);
-
+ testMultiplyAnyArray(testDynamicArray); */
+}
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
